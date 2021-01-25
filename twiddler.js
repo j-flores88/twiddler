5d64bb66190c92eabeee551514d2e614fc3a45d7
Joel Flores <j_flores88@outlook.com>
2021-01-24 19:33:16 -0800

    User Timeline is loaded based on which user is clicked.

--- twiddler.js
+++ twiddler.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+
     const $body = $('body');
     const main = document.getElementsByClassName('.main');
 
     let tweet;
     let $tweet;
     let $userName;
+    let currentUser;
     let stream = streams.home
 
     stream.forEach(tweet => {
@@ -12,8 +14,31 @@ $(document).ready(function(){
         $tweet = $('<div class=tweet></div>')
         $userName = $('<a class=user href="#"></a>')
         $userName.text(`@${tweet.user}`);
-
-        $('.main').append($userName, ': ' + tweet.message);
-        $tweet.appendTo('.main');
+        $userName.on('click', function(e){
+            let userHandle = $(this).text().slice(1);
+            userTimeLine(userHandle)
+        })
+        loadTweet($userName, tweet.message)
     });
+
+    function userTimeLine(user) {
+        $('.main').hide()
+        stream.forEach(tweet => {
+            if(user === tweet.user) {
+                currentUser = $('<a class=user href="#"></a>');
+                currentUser.text(`${tweet.user}`)
+                $tweet = $('<div class=tweet></div>')
+                
+                $('.userTimeLine').append(currentUser, ': ' + tweet.message, ' twiddled at ')
+                
+            }
+            $tweet.appendTo('.userTimeLine')
+        })
+    }
+
+    function loadTweet(user, message, timeStamp) {
+        $('.main').append(user, ': ' + message, ' twiddled at ' + timeStamp);
+        $tweet.appendTo('.main');
+    }
 });
+
